47d28c35324fc67affa902c3213fa56c85ea1f2b
Erki Raiv <erkiraiv@gmail.com>
2021-03-29 22:52:36 +0300

    hide visible secret key

--- settings.py
+++ settings.py
@@ -12,16 +12,20 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+
+# Load the dotenv module
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1e%8#i_dez_o*u-yu#)ni4krim_$1d)u-%(zvf&%te285wjx8#'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

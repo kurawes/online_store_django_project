90feaeadc7fe5994eef2467b4b1d624b070e25e0
Erki Raiv <erkiraiv@gmail.com>
2021-04-03 18:57:56 +0300

    add about and contact page links, user login icon

--- shop/urls.py
+++ shop/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page_view, name='home'),
+    path('about', views.about_page_view, name='about'),
+    path('contact', views.contact_page_view, name='contact'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def home_page_view(request):
     return render(request, 'home.html')
+
+
+def about_page_view(request):
+    return render(request, 'about.html')
+
+
+def contact_page_view(request):
+    return render(request, 'contact.html')

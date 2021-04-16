bfd503586e43322f3aa7063f46453b9f155c5d47
Erki Raiv <erkiraiv@gmail.com>
2021-04-16 15:58:15 +0300

    update shop models and admin panel

--- shop/models.py
+++ shop/models.py
@@ -1,17 +1,33 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=30)
-    objects = models.Manager()
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    # objects = models.Manager()
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    # def get_absolute_url(self):
+    #     return reverse('shop:category_list', args=[self.slug])
 
     def __str__(self):
         return self.name
 
 
 class ProductType(models.Model):
-    name = models.CharField(max_length=30)
-    objects = models.Manager()
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    category = models.ForeignKey(Category, related_name='product_type', on_delete=models.CASCADE)
+    # objects = models.Manager()
+
+    class Meta:
+        verbose_name_plural = "product_types"
+
+    # def get_absolute_url(self):
+    #     return reverse('shop:product_type_list', args=[self.slug])
 
     def __str__(self):
         return self.name
@@ -30,13 +46,22 @@ class Product(models.Model):
         (OUT_OF_STOCK, "Out of stock"),
     ]
     name = models.CharField(max_length=75)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     picture = models.ImageField(upload_to='static/img/')
     availability = models.CharField(max_length=13, choices=AVAILABILITY, default=AVAILABLE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    product_type = models.ForeignKey(ProductType, on_delete=models.DO_NOTHING)
-    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    product_type = models.ForeignKey(ProductType, related_name='product', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_creator')
+    slug = models.SlugField(max_length=255)
+    in_stock = models.BooleanField(default=True)
+    in_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+    updated = models.DateTimeField(auto_now=True, null=True)
     objects = models.Manager()
+
+    class Meta:
+        verbose_name_plural = 'products'
+        ordering = ('-created',)
 
     def __str__(self):
         return self.name

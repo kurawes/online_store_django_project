24a07307acea399adf856b9870eedd206f376f98
Erki Raiv <erkiraiv@gmail.com>
2021-04-17 03:13:18 +0300

    add adding to cart function

--- cart/cart.py
+++ cart/cart.py
@@ -9,5 +9,21 @@ class Cart():
         self.session = request.session
         cart = self.session.get('s_key')
         if 's_key' not in request.session:
-            cart = self.session['s_key'] = {'number': 23516}
+            cart = self.session['s_key'] = {}
         self.cart = cart
+
+    def add(self, product, qty):
+        """
+        Adding and updating the users session data
+        """
+        product_id = product.id
+        if product_id not in self.cart:
+            self.cart[product_id] = {"price": str(product.price), "qty": int(qty)}
+
+        self.session.modified = True   # tells django explicitly that the session has been modified
+
+    def __len__(self):
+        """
+        Get basket data and count qty of items
+        """
+        return sum(item['qty'] for item in self.cart.values())
--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+
+from shop.models import Product
+from .cart import Cart
 
 
 def cart_summary(request):
     return render(request, 'cart.html')
+
+
+def cart_add(request):
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product_qty = request.POST.get('product_qty')
+        product = get_object_or_404(Product, id=product_id)
+        cart.add(product=product, qty=product_qty)
+
+        cart_qty = cart.__len__()
+        response = JsonResponse({'qty': cart_qty})
+        return response

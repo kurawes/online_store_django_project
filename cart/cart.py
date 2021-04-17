0f6a257437d7df36bad3d3f91c6a9e1efc19a905
Erki Raiv <erkiraiv@gmail.com>
2021-04-17 04:17:59 +0300

    add cart view and calculate total sum of cart

--- cart/cart.py
+++ cart/cart.py
@@ -1,3 +1,5 @@
+from shop.models import Product
+from decimal import Decimal
 
 
 class Cart():
@@ -22,8 +24,29 @@ class Cart():
 
         self.session.modified = True   # tells django explicitly that the session has been modified
 
+    def __iter__(self):
+        """"
+        Collect the product_id in the session, query the database and return products
+        """
+        product_ids = self.cart.keys()
+        products = Product.products.filter(id__in=product_ids)
+        cart = self.cart.copy()
+
+        # extend the cart data from the session to add product information (picture, description and so on)
+        for product in products:
+            cart[str(product.id)]['product'] = product
+
+        # loop trough the session data product and capture price and quantity data, add to product information
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])  # change data type to decimal
+            item['total_price'] = item['price'] * item['qty']
+            yield item
+
     def __len__(self):
         """
         Get basket data and count qty of items
         """
         return sum(item['qty'] for item in self.cart.values())
+
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['qty'] for item in self.cart.values())
